df919afe3ec5935d2ff34df8f23e586edd8259fc
Danbob-Airsoft <43638476+Danbob-Airsoft@users.noreply.github.com>
2020-10-26 16:30:34 +0000

    NavMesh for Test level

---
+++ Agars-Island/Assets/Scripts/Enemy Scripts/AITest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AITest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = GameObject.FindGameObjectWithTag("Player").transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
