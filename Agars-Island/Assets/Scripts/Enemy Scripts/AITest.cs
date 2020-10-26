0b34de92b363321ef80c1e04e46f274d3c4e69ed
Danbob-Airsoft <43638476+Danbob-Airsoft@users.noreply.github.com>
2020-10-26 22:39:47 +0000

    Line of sight for AI
    AI will chase player and move to last seen location when in range and line of sight

--- Agars-Island/Assets/Scripts/Enemy Scripts/AITest.cs
+++ Agars-Island/Assets/Scripts/Enemy Scripts/AITest.cs
@@ -5,16 +5,51 @@ using UnityEngine.AI;
 
 public class AITest : MonoBehaviour
 {
+    public GameObject Player;
+    private NavMeshAgent Agent;
+
+    public float SightRange;
+
     // Start is called before the first frame update
     void Start()
     {
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
-        agent.destination = GameObject.FindGameObjectWithTag("Player").transform.position;
+        Agent = GetComponent<NavMeshAgent>();
+        Player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (CheckForSight())
+        {
+            ChasePlayer();
+        }
+    }
+
+    public void ChasePlayer()
+    {
+        Agent.destination = Player.transform.position;
+    }
+
+    public bool CheckForSight()
+    {
+        Vector3 VectToPlayer = Player.transform.position - this.transform.position;
+        //Check if within range
+        if(VectToPlayer.magnitude <= SightRange)
+        {
+            //Check for line of sight
+            RaycastHit HitObject;
+            if(Physics.Raycast(this.transform.position, VectToPlayer, out HitObject))
+            {
+                Debug.DrawRay(this.transform.position, VectToPlayer, Color.blue);
+                if(HitObject.transform.gameObject.tag == "Player")
+                {
+                    return true;
+                }
+            }
+        }
+
+        //Else Return False
+        return false;
     }
 }
