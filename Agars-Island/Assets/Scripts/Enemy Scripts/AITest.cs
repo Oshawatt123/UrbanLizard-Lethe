79ac2b70b375786f513212a94b0c258b656524dc
Danbob-Airsoft <43638476+Danbob-Airsoft@users.noreply.github.com>
2020-11-03 22:05:51 +0000

    Ambush Basis start
    Agent will identify closest object tagged as ambush point and move to it
    Agent can identify when at ambush position

--- Agars-Island/Assets/Scripts/Enemy Scripts/AITest.cs
+++ Agars-Island/Assets/Scripts/Enemy Scripts/AITest.cs
@@ -15,18 +15,26 @@ public class AITest : MonoBehaviour
     public float MaxDirTime;
     private float NextDirTime;
 
+    public GameObject[] AmbushPositions;
+    private bool InAmbush;
+    private bool MovingToAmbush;
+
     // Start is called before the first frame update
     void Start()
     {
         Agent = GetComponent<NavMeshAgent>();
         Player = GameObject.FindGameObjectWithTag("Player");
         NextDirTime = Time.time;
+
+        AmbushPositions = GameObject.FindGameObjectsWithTag("AmbushPos");
+        InAmbush = false;
+        MovingToAmbush = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Check distance to player and line of sight
+        /*//Check distance to player and line of sight
         if (CheckForSight())
         {
             //Set Destination to be Player's location
@@ -37,7 +45,9 @@ public class AITest : MonoBehaviour
         else if(Time.time >= NextDirTime)
         {
             SetDestination(RandomMovement());
-        }
+        }*/
+
+        AmbushBehaviour();
     }
 
     public void SetDestination(Vector3 TargetPos)
@@ -81,5 +91,73 @@ public class AITest : MonoBehaviour
         NavMesh.SamplePosition(RandomDirection, out NavHit, WanderDistance, -1);
 
         return NavHit.position;
+    }
+
+    public void AmbushBehaviour()
+    {
+        //If waiting to Ambush
+        if(InAmbush == true)
+        {
+            //Check if time has passed to leave ambush
+            Debug.Log("Waiting For Ambush");
+        }
+
+        //If already moving to ambush position
+        else if(MovingToAmbush == true)
+        {
+            //Check if arrived at ambush position
+            if (this.transform.position.x == Agent.destination.x && this.transform.position.z == Agent.destination.z)
+            {
+                //Set InAmbush to true and MovingToAmbush to false
+                InAmbush = true;
+                MovingToAmbush = false;
+            }
+
+        }
+
+        //Else Find Closest Ambush point and begin move to
+        else
+        {
+            GameObject ClosestPoint = null;
+            float ClosestDistance = float.MaxValue;
+
+            //Scan each ambush point for closest
+            foreach (GameObject Point in AmbushPositions)
+            {
+                float PathDistance = CalculatePathDistance(Point);
+
+                if (PathDistance < ClosestDistance)
+                {
+                    ClosestDistance = PathDistance;
+                    ClosestPoint = Point;
+                }
+            }
+            SetDestination(ClosestPoint.transform.position);
+            MovingToAmbush = true;
+        }
+    }
+
+    private float CalculatePathDistance(GameObject Point)
+    {
+        NavMeshPath Path = new NavMeshPath();
+        //Calculate path to point
+        if (NavMesh.CalculatePath(this.transform.position, Point.transform.position, NavMesh.AllAreas, Path))
+        {
+            float PathLength = 0;
+            //If Path is valid
+            if (Path.status != NavMeshPathStatus.PathInvalid)
+            {
+                Debug.Log("Valid Path");
+                for (int i = 1; i < Path.corners.Length; ++i)
+                {
+                    PathLength += Vector3.Distance(Path.corners[i - 1], Path.corners[i]);
+                    
+                }
+                return PathLength;
+            }
+        }
+        //If Calculation Fails or Path is incomplete
+        return float.MaxValue;
+
     }
 }
