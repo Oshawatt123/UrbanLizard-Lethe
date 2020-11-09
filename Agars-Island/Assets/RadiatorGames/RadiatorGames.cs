ad2bf159e64b3700cc7091aa9530a83ccac541cf
Oshawatt123 <lewis.arnold@hotmail.co.uk>
2020-11-09 09:53:31 +0000

    Build created

---
+++ Agars-Island/Assets/RadiatorGames/RadiatorGames.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RadiatorGames
+{
+    namespace UI
+    {
+        namespace SwapCanvasGroup
+        {
+            public class GroupSwapper
+            {
+                public static void ShowCanvasGroup(CanvasGroup canvasGroup)
+                {
+                    canvasGroup.alpha = 1.0f;
+                    canvasGroup.interactable = true;
+                    canvasGroup.blocksRaycasts = true;
+                }
+
+                public static void HideCanvasGroup(CanvasGroup canvasGroup)
+                {
+                    canvasGroup.alpha = 0.0f;
+                    canvasGroup.interactable = false;
+                    canvasGroup.blocksRaycasts = false;
+                }
+            }
+        }
+    }
+
+    namespace Sanity
+    {
+        public class SanityTime
+        {
+            public static void SetTimeScale(float timeScale)
+            {
+                Time.timeScale = timeScale;
+            }
+        }
+    }
+}
