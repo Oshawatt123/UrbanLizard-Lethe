825f7131d6c60ba08091290a532f616f35d17491
Danbob-Airsoft <43638476+Danbob-Airsoft@users.noreply.github.com>
2020-11-12 19:16:30 +0000

    Added direct charge behaviour and sanity check

--- Agars-Island/Assets/Scripts/Enemy Scripts/CheckSanity.cs
+++ Agars-Island/Assets/Scripts/Enemy Scripts/CheckSanity.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using BehaviourTree;
+using UnityEngine.AI;
 
-public class CheckSanity : MonoBehaviour
+public class CheckSanity : BT_Behaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Transform Self;
+    private localTree LocalBB;
+    private NavMeshAgent Agent;
+
+    private GameObject Player;
+
+    public CheckSanity(Transform _self)
     {
-        
+        Self = _self;
+        LocalBB = Self.GetComponent<localTree>();
+
+        Agent = Self.GetComponent<NavMeshAgent>();
+
+        Player = GameObject.FindGameObjectWithTag("Player");
     }
 
-    // Update is called once per frame
-    void Update()
+    public override NodeState tick()
     {
-        
+        if(Player.GetComponent<PlayerSanity>().Sanity < 50)
+        {
+            Player.transform.GetChild(1).gameObject.SetActive(true);
+        }
+
+        return NodeState.NODE_FAILURE;
     }
 }
--- Agars-Island/Assets/Scripts/Enemy Scripts/DirectCharge.cs
+++ Agars-Island/Assets/Scripts/Enemy Scripts/DirectCharge.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using BehaviourTree;
+using UnityEngine.AI;
 
-public class DirectCharge : MonoBehaviour
+public class DirectCharge : BT_Behaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Transform Self;
+    private localTree LocalBB;
+    private NavMeshAgent Agent;
+
+    private GameObject Player;
+
+    public DirectCharge(Transform _self)
     {
-        
+        Self = _self;
+        LocalBB = Self.GetComponent<localTree>();
+
+        Agent = Self.GetComponent<NavMeshAgent>();
+        Player = GameObject.FindGameObjectWithTag("Player");
     }
 
-    // Update is called once per frame
-    void Update()
+    public override NodeState tick()
     {
-        
+        //Disable Player Obstacle Cone
+        Player.transform.GetChild(1).gameObject.SetActive(false);
+
+        //Set move to location
+        LocalBB.setMoveToLocation(Player.transform.position);
+
+        return NodeState.NODE_SUCCESS;
     }
 }
