ba71ef7c32b03104ff350e248131c81adb42f551
Oshawatt123 <lewis.arnold@hotmail.co.uk>
2020-11-30 13:04:27 +0000

    MorseCodePuzzleV1

---
+++ Agars-Island/Assets/ResetMorsePuzzle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetMorsePuzzle : MonoBehaviour
+{
+    [SerializeField] private MorseCodeReader reader;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        reader.ResetPuzzle();
+    }
+}
---
+++ Agars-Island/Assets/Switch.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Switch : MonoBehaviour
+{
+    [HideInInspector] public bool On;
+
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public virtual void Init()
+    {
+        anim = GetComponent<Animator>();
+
+        if (!anim)
+            Debug.Log("Anim got");
+    }
+
+    public virtual void Switched()
+    {
+        if (On)
+            anim.SetTrigger("TurnOff");
+        else
+            anim.SetTrigger("TurnOn");
+
+        On = !On;
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        Switched();
+    }
+}
