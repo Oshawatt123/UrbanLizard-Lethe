98853746cde69216ca7c6a3445f3ca79abc65ebe
Oshawatt123 <lewis.arnold@hotmail.co.uk>
2020-12-14 00:49:51 +0000

    Draggable UI Objects

---
+++ Agars-Island/Assets/BlockHorizontal.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(RectTransform))]
+public class BlockHorizontal : MonoBehaviour, IDragHandler
+{
+    private RectTransform rectTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += new Vector2(0, eventData.delta.y);
+    }
+}
---
+++ Agars-Island/Assets/Scripts/UI/DragUIObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(RectTransform))]
+public class DragUIObject : MonoBehaviour, IDragHandler
+{
+
+    public bool lockHorizontal;
+    public bool lockVertical;
+    
+    private RectTransform rectTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += new Vector2(eventData.delta.x * (lockHorizontal == true ? 0 : 1), eventData.delta.y * (lockVertical == true ? 0 : 1));
+    }
+}
