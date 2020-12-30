82eb366a123b48dd8c6b6c6332d178ea7dc942f8
Danbob-Airsoft <43638476+Danbob-Airsoft@users.noreply.github.com>
2020-12-30 16:27:18 +0000

    Main Menu Functions

---
+++ Agars-Island/Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public CanvasGroup MainCanvas;
+    public CanvasGroup ControlsCanvas;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ToControls()
+    {
+        ToggleCanvas(MainCanvas, false);
+        ToggleCanvas(ControlsCanvas, true);
+    }
+
+    public void BackToMenu()
+    {
+        ToggleCanvas(MainCanvas, true);
+        ToggleCanvas(ControlsCanvas, false);
+    }
+
+    private void ToggleCanvas(CanvasGroup ToggleGroup, bool ToggleTo)
+    {
+        ToggleGroup.interactable = ToggleTo;
+        ToggleGroup.blocksRaycasts = ToggleTo;
+        if(ToggleTo == true)
+        {
+            ToggleGroup.alpha = 1;
+        }
+        else
+        {
+            ToggleGroup.alpha = 0;
+        }
+    }
+}
