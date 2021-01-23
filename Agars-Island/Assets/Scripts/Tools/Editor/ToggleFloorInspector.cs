d1d97c30458f4d6e5edda2b09b27d33365a26e27
Oshawatt123 <lewis.arnold@hotmail.co.uk>
2021-01-23 02:05:10 +0000

    More level mesh added, level objects moved into place, ToggleFloor script added

---
+++ Agars-Island/Assets/Scripts/Tools/Editor/ToggleFloorInspector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ToggleFloor))]
+public class ToggleFloorInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        ToggleFloor toggleFloor = (ToggleFloor) target;
+        
+        if (GUILayout.Button("Toggle floor"))
+        {
+            
+            foreach (GameObject gameObject in toggleFloor.FloorObjects)
+            {
+                gameObject.SetActive(toggleFloor.showing);
+            }
+
+            toggleFloor.showing = !toggleFloor.showing;
+        }
+        
+        base.OnInspectorGUI();
+    }
+}
---
+++ Agars-Island/Assets/Scripts/Tools/ToggleFloor.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleFloor : MonoBehaviour
+{
+    public List<GameObject> FloorObjects;
+    [HideInInspector] public bool showing;
+}
