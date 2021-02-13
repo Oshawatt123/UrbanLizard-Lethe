7f6e172f1030723320bb0451c13cf0f992c14096
Oshawatt123 <lewis.arnold@hotmail.co.uk>
2021-02-13 19:25:01 +0000

    More doors added & security camera uses directional light

---
+++ Agars-Island/Assets/CullLights.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CullLights : MonoBehaviour
+{
+
+    [SerializeField] private GameObject sun;
+    [SerializeField] private List<GameObject> lightsToCull;
+    
+    void OnPreCull () {
+        if (sun != null)
+            sun.SetActive(false);
+    }
+
+    void OnPreRender() {
+        if (sun != null)
+            sun.SetActive(false);
+    }
+    void OnPostRender() {
+        if (sun != null)
+            sun.SetActive(true);
+    }
+}
